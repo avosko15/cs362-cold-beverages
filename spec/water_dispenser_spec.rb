c08a955f4bbf540643eb467a4e2302787b688c02
Anna <73409556+avosko15@users.noreply.github.com>
2023-03-21 12:43:51 -0700

    7 test passing, still need to test fill and drain for water reservoir

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
-
+    # it 'drains water from the reservoir into a vessel' do
+    #     reservoir = WaterReservoir.new(10)
+    #     dispenser = WaterDispenser.new(reservoir)
+    #     vessel = Vessel.new('Nalgene', volume: 2)
+    #     dispenser.dispense(vessel)
+    #     expect(reservoir.current_volume).to eq(8)
+    # end
 end
--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+    it 'has a capacity' do
+        water_reservoir = WaterReservoir.new
+        expect(water_reservoir.capacity).to eq(10)
+    end
 
+    it 'is initially empty' do
+        water_reservoir = WaterReservoir.new
+        #expect(water_reservoir.current_water_volume).to eq(0)
+        expect(water_reservoir).to be_empty
+    end
+
+    # it 'is no longer empty when we fill it' do
+    #     water_reservoir.fill
+    #     expect(water_reservoir.current_water_volume).to eq(water_reservoir.capacity)
+    #   end
 end
